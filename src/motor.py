db50d22fa9a1b7d305710c8b710264f4e2333347
jacksonmcl <jmclau05@calpoly.edu>
2022-01-13 09:53:04 -0800

    motor.py template

---
+++ src/motor.py
@@ -0,0 +1,23 @@
+class MotorDriver:
+    '''! 
+    This class implements a motor driver for an ME405 kit. 
+    '''
+
+    def __init__ (self, en_pin, in1pin, in2pin, timer):
+        '''! 
+        Creates a motor driver by initializing GPIO
+        pins and turning the motor off for safety. 
+        @param en_pin (There will be several of these)
+        '''
+        print ('Creating a motor driver')
+
+    def set_duty_cycle (self, level):
+        '''!
+        This method sets the duty cycle to be sent
+        to the motor to the given level. Positive values
+        cause torque in one direction, negative values
+        in the opposite direction.
+        @param level A signed integer holding the duty
+               cycle of the voltage sent to the motor 
+        '''
+        print ('Setting duty cycle to ' + str (level))
