04c3531e4c72eae8020165ae84e4b8483d460840
tbornino <vbornino@calpoly.edu>
2022-01-18 10:28:44 -0800

    Update encoder.py
    modify timer channels in init

--- src/encoder.py
+++ src/encoder.py
@@ -23,20 +23,24 @@ class Encoder:
                     of the encoder.
     """
     
-    def __init__(self, timerID, pin1, pin2):
+    def __init__(self, pin1, pin2, timerID, timerChannel1=1, timerChannel2=2):
         """!@brief      Constructs an encoder object
             @details    The Encoder object stores position and delta values and
                         provides methods to get position or delta and to set
                         position. The hardware timer is set up in this
                         constructor.
-            @param      timerID     Number of timer to use
-            @param      pin1        First encoder pin
-            @param      pin2        Second encoder pin
+            @param      pin1            First encoder pin
+            @param      pin2            Second encoder pin
+            @param      timerID         Timer ID number of timer to use
+            @param      timerChannel1   Channel 1 id number. Default 1
+            @param      timerChannel2   Channel 2 id number. Default 2
         """
         self._timer = pyb.Timer(timerID,period=(_ENC_PERIOD),prescaler=0)
         # Channels are never called in code, but they enable the timer counter 
-        self._channel_1 = self._timer.channel(1,mode=pyb.Timer.ENC_AB,pin=pin1)
-        self._channel_2 = self._timer.channel(2,mode=pyb.Timer.ENC_AB,pin=pin2)
+        self._channel_1 = self._timer.channel(timerChannel1,
+                                              mode=pyb.Timer.ENC_AB,pin=pin1)
+        self._channel_2 = self._timer.channel(timerChannel2,
+                                              mode=pyb.Timer.ENC_AB,pin=pin2)
 
         ## !@brief     Calculated position of the encoder
         #   @details   Most recent calculated position of the motor shaft
@@ -82,7 +86,7 @@ if __name__ == '__main__':
     import time
     
     # Instantiate encoder 1 with default pins and timer
-    enc1 = Encoder(4, pyb.Pin.cpu.B6, pyb.Pin.cpu.B7)
+    enc1 = Encoder(pyb.Pin.cpu.B6, pyb.Pin.cpu.B7, 4)
     
     # Print the encoder read position in ticks every 0.5 seconds
     try:
