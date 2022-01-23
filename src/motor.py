6f979da3e903389ca64c23e24d759e2b83657187
jacksonmcl <jmclau05@calpoly.edu>
2022-01-23 09:29:23 -0800

    motor implementation

--- src/motor.py
+++ src/motor.py
@@ -1,3 +1,4 @@
+import pyb
 class MotorDriver:
     '''! 
     This class implements a motor driver for an ME405 kit. 
@@ -10,6 +11,13 @@ class MotorDriver:
         @param en_pin (There will be several of these)
         '''
         print ('Creating a motor driver')
+        self.en_pin = pyb.Pin(en_pin, pyb.Pin.OUT_PP)
+        self.in1pin = pyb.Pin(in1pin)
+        self.in2pin = pyb.Pin(in2pin)
+        self.timer = timer
+        
+        self.ch1 = self.timer.channel(1, pyb.Timer.PWM, pin=in1pin)
+        self.ch2 = self.timer.channel(2, pyb.Timer.PWM, pin=in2pin)
 
     def set_duty_cycle (self, level):
         '''!
@@ -21,3 +29,27 @@ class MotorDriver:
                cycle of the voltage sent to the motor 
         '''
         print ('Setting duty cycle to ' + str (level))
+        if level < 0 and level >= -100:
+            ch1_level = 0
+            ch2_level = level
+        elif level == 0:
+            ch1_level = 0
+            ch2_level = 0
+        elif level > 0 and level <= 100:
+            ch1_level = level
+            ch2_level = 0
+        else:
+            raise ValueError("level must be between -100 and 100")
+        self.en_pin.high()
+        print("ch1: ", ch1_level, "ch2: ", ch2_level)
+        self.ch1.pulse_width_percent(ch1_level)
+        self.ch2.pulse_width_percent(ch2_level)
+
+
+if __name__ == "__main__":
+    ena_pin = pyb.Pin.board.PA10
+    in1a_pin = pyb.Pin.board.PB4
+    in2a_pin = pyb.Pin.board.PB5
+    tim3 = pyb.Timer(3, freq=20000)
+    moe = MotorDriver(ena_pin, in1a_pin, in2a_pin, tim3)
+    moe.set_duty_cycle(70)
